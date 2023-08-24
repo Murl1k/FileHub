09845bc707f29723a8f3a27e4a67c1fc0ab2242f
Egorchik <egorponomareow@yandex.ru>
2023-08-24 14:20:18 +0500

    Swagger schema and endpoint path changes

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,9 @@
 from djoser.views import UserViewSet
+
+from drf_yasg.openapi import Schema, TYPE_OBJECT, TYPE_NUMBER
 from drf_yasg.utils import swagger_auto_schema
 
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -8,9 +11,17 @@ from .services import get_cached_users_count
 
 
 class CustomUserViewSet(UserViewSet):
-    @swagger_auto_schema(operation_description='Counts all users (updates every 60 seconds)')
+    @swagger_auto_schema(operation_description='Counts all users (updates every 60 seconds)',
+                         responses={
+                             status.HTTP_200_OK: Schema(
+                                 type=TYPE_OBJECT,
+                                 properties={
+                                     'users_count': Schema(type=TYPE_NUMBER)
+                                 }
+                             )
+                         })
     @action(detail=False, methods=['get'])
-    def users_count(self, request):
+    def count_all(self, request):
         users_count = get_cached_users_count()
 
         return Response({'users_count': users_count})
