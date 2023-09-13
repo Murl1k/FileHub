2bfebcdfe48227f423ae81f161846a1017275482
Egorchik <egorponomareow@yandex.ru>
2023-09-13 09:37:26 +0500

    Updated serializers, created CreateFolderSerializer

--- backend/cloud_storage/serializers.py
+++ backend/cloud_storage/serializers.py
@@ -5,5 +5,14 @@ from .models import Folder
 
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'created_at', 'updated_at', 'title', 'size', 'parent_folder')
         model = Folder
+
+
+class FolderCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('title', 'parent_folder', 'storage')
+        model = Folder
+
+    def to_representation(self, instance):
+        return FolderSerializer(instance, context=self.context).data
--- backend/cloud_storage/views.py
+++ backend/cloud_storage/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 
 from .models import Folder
-from .serializers import FolderSerializer
+from .serializers import FolderSerializer, FolderCreateSerializer
 
 
 class FolderViewSet(viewsets.ModelViewSet):
-    serializer_class = FolderSerializer
-
     def get_queryset(self):
         return Folder.objects.filter(storage__owner=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ('create',):
+            return FolderCreateSerializer
+
+        return FolderSerializer
