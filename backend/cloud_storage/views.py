d744ba3e2731777848aaff164a0d8eae3b68fa00
Egor <egorponomareow@yandex.ru>
2023-09-18 16:06:18 +0500

    Added two endpoints: children_folders and children_files

--- backend/cloud_storage/views.py
+++ backend/cloud_storage/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Folder
 from .permissions import IsStorageOwner
-from .serializers import FolderSerializer, FolderCreateEditSerializer
+from .serializers import FolderSerializer, FolderCreateEditSerializer, FileSerializer
+from .services import get_child_folders, get_child_files
 
 
 class FolderViewSet(viewsets.ModelViewSet):
@@ -15,4 +18,29 @@ class FolderViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action in ('create', 'partial_update', 'update'):
             return FolderCreateEditSerializer
+        elif self.action in ('child_files',):
+            return FileSerializer
+
         return FolderSerializer
+
+    @action(detail=True, methods=['get'])
+    def child_folders(self, request, pk):
+        """Get folder's child items (folders, files)"""
+
+        folder = self.get_object()
+        child_folders = get_child_folders(folder)
+
+        serializer = self.get_serializer_class()(child_folders, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def child_files(self, request, pk):
+        """Get files in this folder"""
+
+        folder = self.get_object()
+        child_files = get_child_files(folder)
+
+        serializer = self.get_serializer_class()(child_files, many=True)
+
+        return Response(serializer.data)
