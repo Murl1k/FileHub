6aafc6fea100033bec12821bbe5adc01ef91008c
Egorchik <egorponomareow@yandex.ru>
2023-09-23 11:51:30 +0500

    updated logic of size and used_sized fields

--- backend/cloud_storage/models.py
+++ backend/cloud_storage/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django_minio_backend import MinioBackend
 
 from tools.abstract_models import TimeStampedModel, ShortUUIDModel
+from tools.utils import count_size_in_queryset
 
 User = get_user_model()
 
@@ -27,6 +28,13 @@ class CloudStorage(TimeStampedModel):
     def __str__(self):
         return f"{self.owner} storage"
 
+    def update_used_size(self):
+        """
+        Counts used size of the storage and updates it
+        """
+        self.used_size = count_size_in_queryset(File.objects.filter(storage=self))
+        self.save()
+
 
 class Folder(ShortUUIDModel, TimeStampedModel):
     """
@@ -35,7 +43,7 @@ class Folder(ShortUUIDModel, TimeStampedModel):
     title - folder's title
     parent_folder - parent folder can be blank
     storage - CloudStorage
-    size - used size in bytes. It will be updated on every File creation.
+    size - used size in bytes. It will be updated on every File save or delete.
     """
 
     title = models.CharField(max_length=256)
@@ -45,6 +53,11 @@ class Folder(ShortUUIDModel, TimeStampedModel):
 
     def __str__(self):
         return f"{self.storage.owner} folder ({self.id})"
+
+    def update_size(self):
+        """Updating folder size."""
+        self.size = count_size_in_queryset(File.objects.filter(folder=self))
+        self.save()
 
 
 def get_file_path(instance, filename):
@@ -78,38 +91,16 @@ class File(TimeStampedModel, ShortUUIDModel):
         super(File, self).delete(*args, **kwargs)
 
         # updating size fields on Folder and Storage
-        self.__update_folder_size()
-        self.__update_storage_used_size()
+        self.folder.update_size()
+        self.storage.update_used_size()
 
     def save(self, *args, **kwargs):
         is_adding = self._state.adding
 
         super().save(*args, **kwargs)
 
-        self.__update_folder_size()
-        # if object is just created, we need to update cloud storage used size
+        # updating folder size
+        self.folder.update_size()
+        # if an object is just created, we need to update cloud storage used size.
         if is_adding:
-            self.__update_storage_used_size()
-
-    def __update_folder_size(self):
-        """
-        Counts and updates folder size
-        """
-
-        # changing folder size
-        if self.folder:
-            self.folder.size = self._count_size_in_queryset(File.objects.filter(folder=self.folder))
-            self.folder.save()
-
-    def __update_storage_used_size(self):
-        """
-        Counts size of the storage and updates it
-        """
-        # changing storage used_size
-        self.storage.used_size = self._count_size_in_queryset(File.objects.filter(storage=self.storage))
-        self.storage.save()
-
-    @staticmethod
-    def _count_size_in_queryset(queryset) -> int:
-        """ Counts size of all files in queryset"""
-        return sum([file.file.size for file in queryset])
+            self.storage.update_used_size()
