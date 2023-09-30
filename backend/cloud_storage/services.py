c5a19440911d6b7564ed2d72f0231546cf967a74
Egorchik <egorponomareow@yandex.ru>
2023-09-30 12:46:54 +0500

    Fixed bug with size updating

--- backend/cloud_storage/services.py
+++ backend/cloud_storage/services.py
@@ -1,41 +1,44 @@
-from mptt.utils import get_cached_trees
-
 from .models import Folder
 
 
 def update_ancestors_size_between_two_folders(folder1: Folder, folder2: Folder):
-    """Updates size of all ancestors between two folders"""
+    """Updates the size of the ancestors between two folders until the common ancestor.
+    This is more optimized than just updating each ancestor in the hierarchy.
+    """
     # getting the highest folder (which lvl is lower)
     if folder1.level < folder2.level:
         lowest_folder, highest_folder = folder2, folder1
     else:
         lowest_folder, highest_folder = folder1, folder2
 
-    common_parent = find_common_parent(lowest_folder, highest_folder)
+    common_ancestor = find_first_common_ancestor(lowest_folder, highest_folder)
 
-    # updating every folder in the tree from the lowest one to the common parent
-    while lowest_folder:
+    # updating every folder in the tree from the lowest one until the common ancestor
+    while lowest_folder and lowest_folder != common_ancestor:
         lowest_folder.count_and_update_size()
         lowest_folder = lowest_folder.parent_folder
 
-        if lowest_folder == common_parent:
-            break
+    # if there is no common_ancestor, it means folders don't have the same ancestor, and we need to update
+    # the highest folder's ancestors, because the lowest folder ancestors were already updated in the previous cycle
+    if not common_ancestor:
+        update_ancestors_folders_size(highest_folder)
 
 
 def update_ancestors_folders_size(folder: Folder):
     """Updates the size of all ancestors """
-    while folder:
+    ancestors = folder.get_ancestors(include_self=True, ascending=True)
+
+    for folder in ancestors:
         folder.count_and_update_size()
-        folder = folder.parent_folder
 
 
-def find_common_parent(lowest_folder: Folder, highest_folder: Folder):
-    """Finds first common parent of two folders. If there is not, it will return None"""
-    tree = get_cached_trees((highest_folder, lowest_folder))
+def find_first_common_ancestor(lowest_folder: Folder, highest_folder: Folder):
+    """Finds the first common ancestor of two folders. If there is not, it will return None"""
+    highest_folder_tree = highest_folder.get_ancestors(include_self=True, ascending=True)
+    lowest_folder_tree = lowest_folder.get_ancestors(include_self=True, ascending=True)
 
-    if not tree:
+    try:
+        # getting the intersection of two lists and getting the first element which is the first common parent
+        return list(set(lowest_folder_tree) & set(highest_folder_tree))[0]
+    except IndexError:
         return None
-
-    common_ancestor = tree[0]
-
-    return common_ancestor
