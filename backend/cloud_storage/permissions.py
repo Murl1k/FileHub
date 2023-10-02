726667fca8fa372d66a50f502b66ce21e0748a21
Egorchik <egorponomareow@yandex.ru>
2023-10-02 12:54:47 +0500

    Added docstring

--- backend/cloud_storage/permissions.py
+++ backend/cloud_storage/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 
 class IsStorageOwner(BasePermission):
-    """Checks if the user is the folder's storage owner"""
+    """Checks if the user is the storage owner"""
 
     def has_object_permission(self, request, view, obj):
         if request.user == obj.storage.owner:
@@ -11,5 +11,6 @@ class IsStorageOwner(BasePermission):
 
 
 class IsStorageOwnerOrIsFilePublic(IsStorageOwner):
+    """Checks if the user is the storage owner or the file is public"""
     def has_object_permission(self, request, view, obj):
         return super().has_object_permission(request, view, obj) or obj.is_public
