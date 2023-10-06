592c4b5c8722fa2dd5fd95ff119db105852dda47
Egorchik <egorponomareow@yandex.ru>
2023-10-06 16:21:16 +0500

    Updated celery config

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-app = Celery('backend')
+app = Celery('backend', backend=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings')
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
