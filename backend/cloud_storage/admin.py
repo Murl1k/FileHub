b97c43c4f16296d706720e971160e4f8b760760f
Egorchik <egorponomareow@yandex.ru>
2023-10-20 21:41:32 +0500

    added jazzmin admin panel

--- backend/cloud_storage/admin.py
+++ backend/cloud_storage/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django_mptt_admin.admin import DjangoMpttAdmin
-
 from .models import CloudStorage, Folder, File
 
 admin.site.register(CloudStorage)
 admin.site.register(File)
-admin.site.register(Folder, DjangoMpttAdmin)
+admin.site.register(Folder)
 
